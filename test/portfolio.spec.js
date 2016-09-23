002b4ae7862f00fb2e482fad59f5d070cbe6689d
Sergio Cruz <hello@sergiocruz.me>
2016-09-22 22:11:35 -0400

    Updated tests

--- test/portfolio.spec.js
+++ test/portfolio.spec.js
@@ -26,21 +26,21 @@ describe('The webpage', () => {
 
     it('should have changed the contents of `.navbar-brand` @navbar-brand', () => {
       const el = navbar.querySelector('.navbar-brand');
-      assert.isOk(el, 'Our `.navbar` needs a `.navbar-brand` element.');
-      assert.notEqual('brand', el.textContent.trim().toLowerCase(), 'The `.navbar-brand` element needs a text that is different than the default "Brand".');
+      assert(el, 'Our `.navbar` needs a `.navbar-brand` element.');
+      assert('brand' !== el.textContent.trim().toLowerCase(), 'The `.navbar-brand` element needs a text that is different than the default "Brand".');
     });
 
     it('should have have changed menu items @navbar-menu', () => {
       const menuItems = Array.from(navbar.querySelectorAll('.nav li a'));
-      assert.isAtLeast(menuItems.length, 2, 'Our menu needs at least 2 `li` elements.');
+      assert(menuItems.length >= 2, 'Our menu needs at least 2 `li` elements.');
 
       // Does home link exist?
       const homeLink = menuItems.find(el => /home/i.test(el.textContent));
-      assert.isOk(homeLink, 'Our menu needs at least one link that reads "Home".');
+      assert(homeLink, 'Our menu needs at least one link that reads "Home".');
 
       // Does about link exist?
       const aboutLink = menuItems.find(el => /about/i.test(el.textContent));
-      assert.isOk(aboutLink, 'Our menu needs at least one link that reads "About".');
+      assert(aboutLink, 'Our menu needs at least one link that reads "About".');
     });
   });
 
@@ -54,24 +54,27 @@ describe('The webpage', () => {
     beforeEach(() => carousel = doc.querySelector('.carousel'));
 
     it('should exist @carousel', () => {
-      assert.isOk(carousel, 'We need a `.carousel` element.');
+      assert(carousel, 'We need a `.carousel` element.');
     });
 
     it('should have at least 3 items and have them modified @carousel-items', () => {
       const items = Array.from(carousel.querySelectorAll('.item'));
-      assert.isAtLeast(items.length, 3, 'Our carousel needs at least 3 `.item` elements.');
+      assert(items.length >= 3, 'Our carousel needs at least 3 `.item` elements.');
 
       items.forEach(item => {
 
         // Does each item contain an `<h1>` tag?
         const h1 = item.querySelector('h1');
-        assert.isOk(h1, 'Each carousel item needs an `h1` element.');
-        assert.notEqual(h1.textContent.trim(), 'Example headline', 'Every `h1` inside the carousel items need a customized text content.');
+        assert(h1, 'Each carousel item needs an `h1` element.');
+        assert(h1.textContent.trim() !== 'Example headline', 'Every `h1` inside the carousel items need a customized text content.');
 
         // Does each item contain an `<p>` tag?
         const p = item.querySelector('p');
-        assert.isOk(p, 'Each carousel item needs a `p` element.');
-        assert.isNotOk(p.textContent.trim().startsWith('Cras justo odio', 'Every `p` element inside carousel items need a customized text content.'));
+        assert(p, 'Each carousel item needs a `p` element.');
+        assert(
+          !p.textContent.trim().startsWith('Cras justo odio'),
+          'Every `p` element inside carousel items need a customized text content.'
+        );
       });
     });
   });
@@ -85,17 +88,17 @@ describe('The webpage', () => {
     beforeEach(() => marketing = doc.querySelector('.marketing'));
 
     it('should exist @marketing', () => {
-      assert.isOk(marketing, 'We need a `.marketing` element.');
+      assert(marketing, 'We need a `.marketing` element.');
     });
 
     it('should have a cointainer @marketing', () => {
       const el = marketing.querySelector('.container');
-      assert.isOk(el, 'Our `.marketing` element needs a `.container` element.');
+      assert(el, 'Our `.marketing` element needs a `.container` element.');
     });
 
     it('should have a row @marketing', () => {
       const el = marketing.querySelector('.container .row');
-      assert.isOk(el, 'Our marketing `.container` needs a `.row` element.');
+      assert(el, 'Our marketing `.container` needs a `.row` element.');
     });
 
     describe('columns', () => {
@@ -108,15 +111,15 @@ describe('The webpage', () => {
       });
 
       it('should exist at least 3 @marketing-columns', () => {
-        assert.equal(columns.length, 3, 'Our `.row` element needs at least 3 column elements.');
+        assert(columns.length === 3, 'Our `.row` element needs at least 3 column elements.');
       });
 
       it('should have an icon with the glyphicon icon class @marketing-columns', () => {
         columns.forEach(column => {
           const icon = column.querySelector('.glyphicon');
           const iconClasses = Array.from(icon.classList);
-          assert.isOk(icon, 'Every marketing row column needs an icon.');
-          assert.isOk(
+          assert(icon, 'Every marketing row column needs an icon.');
+          assert(
             iconClasses.find(className => className.startsWith('glyphicon-')),
             'Our marketing icons needs a class that starts with `glaphicon-` to describe which icon graphic it should contain.'
           );
@@ -126,16 +129,16 @@ describe('The webpage', () => {
       it('should have a non-empty `<h2>` title @marketing-columns', () => {
         columns.forEach(column => {
           const h2 = column.querySelector('h2');
-          assert.isOk(h2, 'Every marketing column needs an `h2` element.');
-          assert.isOk(h2.textContent.trim() !== '', 'Our marketing column `h2` elements cannot be empty.');
+          assert(h2, 'Every marketing column needs an `h2` element.');
+          assert(h2.textContent.trim() !== '', 'Our marketing column `h2` elements cannot be empty.');
         })
       });
 
       it('should have a non-empty paragraph @marketing-columns', () => {
         columns.forEach(column => {
           const p = column.querySelector('p');
-          assert.isOk(p, 'Every marketing column needs a `p` element.');
-          assert.isOk(p.textContent.trim() !== '', 'Our marketing column `p` elements cannot be empty.');
+          assert(p, 'Every marketing column needs a `p` element.');
+          assert(p.textContent.trim() !== '', 'Our marketing column `p` elements cannot be empty.');
         });
       });
     });
