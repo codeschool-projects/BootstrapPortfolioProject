9bfebfa5e7b5f81dca644bfcd7e85ab26962c004
MelonMonkey2017 <703837@pdsb.net>
2017-04-16 09:01:29 -0400

    Something cool

--- src/client.js
+++ src/client.js
@@ -1,3 +1,4 @@
-function welcome() {
-  alert('Welcome back, Carl!');
+function viewPost() {
+  var w = window.open("");
+  w.document.writeln("<!DOCTYPE html><html><head><meta charset='utf-8'><title></title></head><body><h1>Hello World!<h1></body></html>");
 }
